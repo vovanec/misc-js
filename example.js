0d866871b0bd500c1c19fc8033dc5595bfbf3573
Vovan Kuznetsov <vkuznet@juniper.net>
2015-01-11 00:22:49 -0800

    mode minor changes

--- example.js
+++ example.js
@@ -12,4 +12,7 @@
     utils.log.info(
         _.chain([1, 2, 3, 4, 5]).map(_.l('x -> x * 2')).value()
     );
+    utils.log.info(
+        _.chain([1, 2, 3, 4, 5]).map(utils.l('x -> x * 2')).value()
+    );
 }());
--- misc.js
+++ misc.js
@@ -1,14 +1,37 @@
+/* Copyright (c) 2015 year, Volodymyr Kuznetsov
+
+ Permission to use, copy, modify, and/or distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
 
 'use strict';
 
 var _ = require('underscore');
 
 
+var _make_logger = function (level) {
+    return function (message) {
+        console.log(Date.now().toString() + ' [' + level + ']: ' + message);
+    };
+};
+
+
 var log = {
-    debug: console.log.bind(console),
-    info:  console.log.bind(console),
-    warn:  console.log.bind(console),
-    error: console.log.bind(console)
+    debug:    _make_logger('DEBUG'),
+    info:     _make_logger('INFO'),
+    warn:     _make_logger('WARNING'),
+    warning:  _make_logger('WARNING'),
+    error:    _make_logger('ERROR'),
+    critical: _make_logger('CRITICAL')
 };
 
 
@@ -21,53 +44,52 @@ var construct = function (constructor, args) {
 };
 
 
-var LambdaDefinitionError = function (message) {
+var LambdaExpressionError = function (message) {
     Error.apply(this, arguments);
-    this.name = 'LambdaDefinitionError';
+    this.name = 'LambdaExpressionError';
     this.message = message;
 };
 
-LambdaDefinitionError.prototype = Error.prototype;
-LambdaDefinitionError.prototype.constructor = LambdaDefinitionError;
+LambdaExpressionError.prototype = Error.prototype;
+LambdaExpressionError.prototype.constructor = LambdaExpressionError;
 
 
 var lambda = function (definition) {
     var lambda_expr_parts = definition.match(/^(.+)\s*->\s*(.+)$/);
     if (!lambda_expr_parts || lambda_expr_parts.length !== 3) {
-        throw new LambdaDefinitionError(
+        throw new LambdaExpressionError(
             'Invalid lambda definition: ' + definition
         );
     }
-    var func_args = _.filter(
-        lambda_expr_parts[1].trim().split(/\s+/),
-        function (elem) {
-            return !_.isEmpty(elem);
-        }
-    );
+    var lambda_args = _.filter(lambda_expr_parts[1].split(/\s+/),
+        function (arg) {
+            return !_.isEmpty(arg);
+        });
 
-    if (func_args.length === 0) {
-        throw new LambdaDefinitionError(
+    if (lambda_args.length === 0) {
+        throw new LambdaExpressionError(
             'Lambda function arguments list is empty'
         );
     }
 
-    var func_body = lambda_expr_parts[2].trim();
-    if (!func_body) {
-        throw new LambdaDefinitionError(
+    var lambda_body = lambda_expr_parts[2].trim();
+    if (!lambda_body) {
+        throw new LambdaExpressionError(
             'Lambda function body is empty'
         );
     }
 
-    func_args.push('return ' + func_body);
-    return construct(Function, func_args);
+    return construct(Function, lambda_args.concat(['return ' + lambda_body]));
 };
-
-exports.log = log;
-exports.construct = construct;
-exports.lambda = exports.l = lambda;
-exports.LambdaDefinitionError = LambdaDefinitionError;
 
 
 (function () {
-    require('underscore').mixin({'lambda': lambda, 'l': lambda});
+    _.mixin({'lambda': lambda, 'l': lambda});
+    _.extend(exports, {
+        log: log,
+        construct: construct,
+        lambda: lambda,
+        l: lambda,
+        LambdaExpressionError: LambdaExpressionError
+    });
 }());
