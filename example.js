89614d38c1ebc87d12e917be9e39b2c46359e0ef
Vovan Kuznetsov <vkuznet@juniper.net>
2015-01-11 00:27:38 -0800

    version bump

--- example.js
+++ example.js
@@ -9,10 +9,13 @@
     var utils = require('./misc'),
         _ = require('underscore');
 
-    utils.log.info(
-        _.chain([1, 2, 3, 4, 5]).map(_.l('x -> x * 2')).value()
-    );
+    // Invoke lambda from utils package
     utils.log.info(
         _.chain([1, 2, 3, 4, 5]).map(utils.l('x -> x * 2')).value()
+    );
+
+    // Invoke lambda mixed into underscore package
+    utils.log.info(
+        _.chain([1, 2, 3, 4, 5]).map(_.l('x -> x * 2')).value()
     );
 }());
